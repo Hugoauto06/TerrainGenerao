1ffb9fada99e3756b3b31826c7b1903d3c21cda4
Savuth-S <SavuthS4ns@protonmail.com>
2021-03-03 13:54:06 -0500

    Creacion de un archivo para la clase Player

--- TerrainGenerator/Program.cs
+++ TerrainGenerator/Program.cs
@@ -2,18 +2,6 @@
 
 namespace TerrainGenerator
 {
-    public class Player
-    {
-        public int X { get; set; }
-        public int Y { get; set; }
-
-        public Player(int x, int y)
-        {
-            X = x;
-            Y = y;
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
